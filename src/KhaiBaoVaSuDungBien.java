b68877b1f0ded9b566fdddf21f72488b6bdd718f
akityty <congnv.9803@gmail.com>
2019-07-23 14:07:21 +0700

    ok

---
+++ src/KhaiBaoVaSuDungBien.java
@@ -0,0 +1,16 @@
+public class KhaiBaoVaSuDungBien {
+    public static void main(String[] args) {
+        int i = 1;
+        float f = 2.2f;
+        double d = 2.3;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println("i = "+ i);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println("b = "+ b);
+        System.out.println("c = "+ c);
+        System.out.println("string = "+ s);
+    }
+}
